b11e309055e02db2914bf505fcf311819e3435c9
atlantique <helloatlantique@gmail.com>
2016-01-22 15:30:25 -0600

    AOT

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject clj-twilio "0.2.0"
+(defproject clj-twilio "0.2.1"
   :description "Clojure Twilio API"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prxml "1.3.0"]]
-  :jar-name "clj-twilio.jar")
+  :jar-name "clj-twilio.jar"
+  :aot [twilio.core
+        twilio.twiml.core
+        twilio.twiml.sms
+        twilio.twiml.voice])
